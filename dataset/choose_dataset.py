e31d62b2f5783992f93d6ebb8d06a90467930738
justpers <nunajiyo@naver.com>
2025-05-19 14:10:49 +0000

    MNIST 데이터 다운경로변경

--- dataset/choose_dataset.py
+++ dataset/choose_dataset.py
@@ -1,13 +1,18 @@
-from dataset.mnist import MNIST
+from torchvision.datasets import MNIST
+from torchvision import transforms
+
 from dataset.CUB200 import CUB_200
 from dataset.ConText import ConText, MakeList, MakeListImage
 from dataset.transform_func import make_transform
 
-
 def select_dataset(args):
     if args.dataset == "MNIST":
-        dataset_train = MNIST('./data/mnist', train=True, download=True, transform=make_transform(args, "train"))
-        dataset_val = MNIST('./data/mnist', train=False, transform=make_transform(args, "val"))
+        transform = transforms.Compose([
+            transforms.Resize((args.img_size, args.img_size)),
+            transforms.ToTensor()
+        ])
+        dataset_train = MNIST(root="./data/mnist", train=True, transform=transform, download=True)
+        dataset_val   = MNIST(root="./data/mnist", train=False, transform=transform, download=True)
         return dataset_train, dataset_val
     if args.dataset == "CUB200":
         dataset_train = CUB_200(args, train=True, transform=make_transform(args, "train"))
@@ -25,4 +30,3 @@ def select_dataset(args):
         return dataset_train, dataset_val
 
     raise ValueError(f'unknown {args.dataset}')
-
