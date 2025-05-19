7016ae569efe007c74ead46250bd5d0838d66651
justpers <nunajiyo@naver.com>
2025-05-19 14:11:02 +0000

    튜플형태 대응 가능하도록 변경

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import torch
 import tools.calculate_tool as cal
 from tqdm.auto import tqdm
+from collections.abc import Mapping
 
 
 def train_one_epoch(model, data_loader, optimizer, device, record, epoch):
@@ -21,31 +22,40 @@ def calculation(model, mode, data_loader, device, record, epoch, optimizer=None)
     running_att_loss = 0.0
     running_log_loss = 0.0
     print("start " + mode + " :" + str(epoch))
+
     for i_batch, sample_batch in enumerate(tqdm(data_loader)):
-        inputs = sample_batch["image"].to(device, dtype=torch.float32)
-        labels = sample_batch["label"].to(device, dtype=torch.int64)
+        # 딕셔너리 형태 or 튜플 형태 대응
+        if isinstance(sample_batch, Mapping):  # dict-like
+            inputs = sample_batch["image"].to(device, dtype=torch.float32)
+            labels = sample_batch["label"].to(device, dtype=torch.int64)
+        elif isinstance(sample_batch, (list, tuple)) and len(sample_batch) == 2:
+            inputs, labels = sample_batch
+            inputs = inputs.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.int64)
+        else:
+            raise TypeError(f"Unsupported sample_batch format: {type(sample_batch)}")
 
         if mode == "train":
             optimizer.zero_grad()
+
         logits, loss_list = model(inputs, labels)
         loss = loss_list[0]
+
         if mode == "train":
             loss.backward()
-            # clip_gradient(optimizer, 1.1)
             optimizer.step()
 
         a = loss.item()
         running_loss += a
-        if len(loss_list) > 2: # For slot training only
+        if len(loss_list) > 2:  # For slot training only
             running_att_loss += loss_list[2].item()
             running_log_loss += loss_list[1].item()
         running_corrects += cal.evaluateTop1(logits, labels)
-        # if i_batch % 10 == 0:
-        #     print("epoch: {} {}/{} Loss: {:.4f}".format(epoch, i_batch, L-1, a))
-    epoch_loss = round(running_loss/L, 3)
-    epoch_loss_log = round(running_log_loss/L, 3)
-    epoch_loss_att = round(running_att_loss/L, 3)
-    epoch_acc = round(running_corrects/L, 3)
+
+    epoch_loss = round(running_loss / L, 3)
+    epoch_loss_log = round(running_log_loss / L, 3)
+    epoch_loss_att = round(running_att_loss / L, 3)
+    epoch_acc = round(running_corrects / L, 3)
     record[mode]["loss"].append(epoch_loss)
     record[mode]["acc"].append(epoch_acc)
     record[mode]["log_loss"].append(epoch_loss_log)
